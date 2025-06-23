45fea9504e0207dd57edf504fb35a2abc561d4d8
willian <54144699+willmont1982@users.noreply.github.com>
2025-06-23 09:28:11 -0300

    Update Pedido.java

--- eucomida/src/main/java/com/eucomida/model/Pedido.java
+++ eucomida/src/main/java/com/eucomida/model/Pedido.java
@@ -20,5 +20,39 @@ public class Pedido {
         EM_ANDAMENTO, ENTREGUE, CANCELADO
     }
 
-    // Getters e setters
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+}
+
 }
