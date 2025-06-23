a572b34d7184127ea89da00b6350c33eaed4cbb9
willian <54144699+willmont1982@users.noreply.github.com>
2025-06-23 09:29:07 -0300

    Update Usuario.java

--- eucomida/src/main/java/com/eucomida/model/Usuario.java
+++ eucomida/src/main/java/com/eucomida/model/Usuario.java
@@ -12,5 +12,38 @@ public class Usuario {
     private String senha;
     private String role;
 
-    // Getters e setters
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
+
